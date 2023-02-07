0eaa9862a7865471f95b0441db2b60ccf46570a2
yscope <thienyglobe@gmail.com>
2023-02-07 18:45:12 +0700

    fix unused grads

--- experiments/spider_dg/bayesian_meta_train.py
+++ experiments/spider_dg/bayesian_meta_train.py
@@ -110,9 +110,9 @@ class BayesianMetaTrainer(bayesian_meta_train.BMetaTrainer):
     ):
         with self.model_random:
             
-            for p in self.model.parameters():
-                if p.grad is None:
-                    p.grad = torch.zeros_like(p)
+            # for p in self.model.parameters():
+            #     if p.grad is None:
+            #         p.grad = torch.zeros_like(p)
             model_encoder_params = []
             for i in range(self.train_config.num_particles):
                 model_encoder_params.append(list(self.model.list_of_encoders[i].parameters()))
