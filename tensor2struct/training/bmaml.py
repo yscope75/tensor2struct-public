33f1fd3c433059e3d043154f8e869a183473e2c8
yscope <thienyglobe@gmail.com>
2023-04-06 16:38:43 +0700

    optimize bmaml code

--- tensor2struct/training/bmaml.py
+++ tensor2struct/training/bmaml.py
@@ -302,8 +302,6 @@ class BayesModelAgnosticMetaLearning(nn.Module):
                     
         final_loss = sum(inner_loss)/self.num_particles + mean_outer_loss.item()
         ret_dic["loss"] = final_loss
-        del inner_model
-        gc.collect()
         
         return ret_dic
     
