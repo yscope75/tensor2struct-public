3098dd8e0d17a77e22e677b19dded1c455792768
yscope <thienyglobe@gmail.com>
2023-05-30 14:56:18 +0700

    refactor bmaml train

--- tensor2struct/training/bmaml.py
+++ tensor2struct/training/bmaml.py
@@ -525,7 +525,7 @@ class BayesModelAgnosticMetaLearning(nn.Module):
         final_loss = sum(inner_loss)/self.num_particles + sum(loss_over_pars)
         ret_dic["loss"] = final_loss
         # del inner_encoders
-        del inner_grads
+        del aligner_grads
         # del inner_decoder
         import gc
         gc.collect()
