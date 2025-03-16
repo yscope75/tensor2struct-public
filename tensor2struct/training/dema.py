d4b8bf4c79f6c265758b3d9c0148cb50415ebb9e
yscope <thienyglobe@gmail.com>
2025-03-16 17:36:09 +0700

    update condition input for conditional layer norm in training

--- tensor2struct/training/dema.py
+++ tensor2struct/training/dema.py
@@ -102,6 +102,8 @@ class DeepEnsembleModelAgnostic(nn.Module):
             # for single input source domain
             enc_states = []
             for idx, (enc_input, plm_out) in enumerate(zip(enc_input_list, plm_output)):
+                (q_enc, col_enc, tab_enc) = plm_output
+                condition = torch.mean(torch.cat((q_enc, col_enc, tab_enc), dim=0), dim=0, keepdim=True)
                 relation = model.schema_linking(enc_input)
                 (
                     q_enc_new_item,
@@ -109,7 +111,8 @@ class DeepEnsembleModelAgnostic(nn.Module):
                     t_enc_new_item,
                 ) = model.list_of_encoders[i](enc_input, 
                                                     plm_out,
-                                                    relation)
+                                                    relation,
+                                                    condition)
                 # attention memory 
                 memory = []
                 include_in_memory = model.list_of_encoders[i].include_in_memory
